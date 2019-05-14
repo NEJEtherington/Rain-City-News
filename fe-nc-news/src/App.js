8b1f043f6d72bd06f1553624b40d00b09c07f067
Nick Etherington <nicketherington@Nicks-MacBook.local>
2019-05-14 14:14:01 +0100

    updated Header for Topics button

--- fe-nc-news/src/App.js
+++ fe-nc-news/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
-import Header from "./Header";
+import Header from "./Components/Header";
+import Articles from "./Components/Articles";
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <Articles />
       </div>
     );
   }
