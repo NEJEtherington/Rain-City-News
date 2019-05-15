cb5613eafc5784797dfdb3ca85cc4df6c7a5f4ae
Nick Etherington <nicketherington@Nicks-MacBook.local>
2019-05-15 17:22:34 +0100

    updated header styling, commented out filter by form

--- fe-nc-news/src/Components/Header.jsx
+++ fe-nc-news/src/Components/Header.jsx
@@ -6,21 +6,23 @@ class Header extends React.Component {
   render() {
     return (
       <div>
-        <ul className="navbar">
-          <Link to="/articles">
-            <li className="ncnews">NC News</li>
-          </Link>
-          <Link to="/topics">
-            <li className="header">Topics </li>
-          </Link>
-          <li className="header">Login/Sign Up</li> <li>Create Post</li>
-          <li className="header">
+        <nav>
+          <ul className="navbar">
+            <Link to="/articles">
+              <li className="ncnews">NC News</li>
+            </Link>
+            <Link to="/topics">
+              <li className="header">Topics </li>
+            </Link>
+            <li className="header">Login/Sign Up</li> <li>Create Post</li>
+          </ul>
+        </nav>
+        {/* <li className="header">
             <form>
               <input type="text" placeholder="Filter by author or topic" />
               <button>Submit</button>
             </form>
-          </li>
-        </ul>
+          </li> */}
       </div>
     );
   }
