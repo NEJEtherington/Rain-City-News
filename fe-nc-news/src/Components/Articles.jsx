47e11e70562dfce1fc8243797d3e08ecc818e295
Nick Etherington <nicketherington@Nicks-MacBook.local>
2019-05-17 12:23:48 +0100

    added sortBy functionality

--- fe-nc-news/src/Components/Articles.jsx
+++ fe-nc-news/src/Components/Articles.jsx
@@ -5,19 +5,19 @@ import ArticleCard from "./ArticleCard";
 
 class Articles extends React.Component {
   state = {
-    articles: []
+    articles: [],
+    sortBy: null
   };
 
   render() {
-    // console.log("Articles", this.props);
     return (
       <div>
         <div className="dropdown">
           <h5 className="dropbtn">Sort by</h5>
           <div className="dropdown-content">
-            <h5>Title</h5>
-            <h5>Author</h5>
-            <h5>Date</h5>
+            <button onClick={this.handlePostedClick}>Posted</button>
+            <button onClick={this.handleCommentsClick}>Comments</button>
+            <button onClick={this.handleVotesClick}>Votes</button>
           </div>
         </div>
         {this.state.articles.map(article => {
@@ -33,11 +33,32 @@ class Articles extends React.Component {
     );
   }
 
+  //refactor the three functions below into a single function?
+  handleCommentsClick = () => {
+    this.setState({ sortBy: "comment_count" });
+  };
+
+  handlePostedClick = () => {
+    this.setState({ sortBy: "created_at" });
+  };
+
+  handleVotesClick = () => {
+    this.setState({ sortBy: "votes" });
+  };
+
   componentDidMount() {
     const queries = { topic: this.props.topic };
     getArticles(queries).then(articles => {
       this.setState({ articles });
     });
+  }
+
+  componentDidUpdate(_, prevState) {
+    const query = { sort_by: this.state.sortBy };
+    this.state.sortBy !== prevState.sortBy &&
+      getArticles(query).then(articles => {
+        this.setState({ articles });
+      });
   }
 }
 
