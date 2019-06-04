25b8fdf666c5cdc656a56c1fc1a024d2e16f871f
Nick Etherington <nicketherington@Nicks-MacBook.local>
2019-06-04 15:50:47 +0100

    added user avatars

--- fe-nc-news/src/Components/SingleArticle.jsx
+++ fe-nc-news/src/Components/SingleArticle.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import { getSingleArticle } from "../Api";
+import GoogleFontLoader from "react-google-font-loader";
+import { getSingleArticle, getUser } from "../Api";
 import Comments from "./Comments";
 import Voter from "./Voter";
 
 class SingleArticle extends React.Component {
   state = {
-    article: null
+    article: null,
+    userAvatar: null
   };
 
   render() {
@@ -13,12 +15,19 @@ class SingleArticle extends React.Component {
     return (
       <div>
         {article ? (
-          <div>
+          <>
+            {this.state.userAvatar && (
+              <img
+                className="avatar"
+                alt="avatar"
+                src={this.state.userAvatar}
+              />
+            )}
             <h5>Author: {article.author}</h5>
             <h5>Posted: {new Date(article.created_at).toLocaleString()}</h5>
             <h3>{article.title}</h3>
             <h5>Topic: {article.topic}</h5>
-            <p>{article.body}</p>
+            <p className="text">{article.body}</p>
             {this.props.username && (
               <Voter
                 votes={article.votes}
@@ -26,11 +35,39 @@ class SingleArticle extends React.Component {
                 type="article"
               />
             )}
-            <h4>Comments</h4>
+            <GoogleFontLoader
+              fonts={[
+                {
+                  font: "IM Fell DW Pica SC",
+                  weights: [400, "400i"]
+                }
+              ]}
+            />
+            <h3
+              className="center"
+              style={{ fontFamily: "IM Fell DW Pica SC, monospaced" }}
+            >
+              Comments
+            </h3>
             <Comments id={this.props.id} username={this.props.username} />
-          </div>
+          </>
         ) : (
-          <p>loading...</p>
+          <>
+            <GoogleFontLoader
+              fonts={[
+                {
+                  font: "IM Fell DW Pica SC",
+                  weights: [400, "400i"]
+                }
+              ]}
+            />
+            <h2
+              style={{ fontFamily: "IM Fell DW Pica SC, monospaced" }}
+              className="center"
+            >
+              loading...
+            </h2>
+          </>
         )}
       </div>
     );
@@ -40,6 +77,15 @@ class SingleArticle extends React.Component {
     getSingleArticle(this.props.id).then(article => {
       this.setState({ article });
     });
+  }
+
+  componentDidUpdate() {
+    if (this.state.article) {
+      const username = this.state.article.author;
+      getUser(username).then(user =>
+        this.setState({ userAvatar: user.avatar_url })
+      );
+    }
   }
 }
 
