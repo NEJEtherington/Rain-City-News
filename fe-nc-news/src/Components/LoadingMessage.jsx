9755d1e55aa436c1c719ae75f4e27fa0363e0b90
Nick Etherington <nicketherington@Nicks-MacBook.local>
2019-06-05 14:56:40 +0100

    added font

--- fe-nc-news/src/Components/LoadingMessage.jsx
+++ fe-nc-news/src/Components/LoadingMessage.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import GoogleFontLoader from "react-google-font-loader";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCloudRain } from "@fortawesome/free-solid-svg-icons";
 
 export default function LoadingMessage() {
   return (
     <div id="loadingMessage">
+      <GoogleFontLoader
+        fonts={[
+          {
+            font: "IM Fell DW Pica SC",
+            weights: [400, "400i"]
+          }
+        ]}
+      />
       <FontAwesomeIcon className="icon" icon={faCloudRain} size="lg" spin />
       <h3 style={{ fontFamily: "IM Fell DW Pica SC, monospaced" }}>
         loading...
