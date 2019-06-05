a5c6cbfeb48154d629519164aa19c43611f6a8d7
Nick Etherington <nicketherington@Nicks-MacBook.local>
2019-06-05 09:25:28 +0100

    disabled post comment button if no text input, successfully posted comment message and  button styling

--- fe-nc-news/src/Components/PostComment.jsx
+++ fe-nc-news/src/Components/PostComment.jsx
@@ -1,30 +1,62 @@
 import React from "react";
+import GoogleFontLoader from "react-google-font-loader";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCloudRain } from "@fortawesome/free-solid-svg-icons";
 import { postComment } from "../Api";
 
 class PostComment extends React.Component {
   state = {
-    commentInput: ""
+    commentInput: "",
+    newPost: false
   };
 
   render() {
     return (
       <div>
+        <GoogleFontLoader
+          fonts={[
+            {
+              font: "IM Fell DW Pica SC",
+              weights: [400, "400i"]
+            },
+            {
+              font: "IM Fell DW Pica",
+              weights: [400, "400i"]
+            }
+          ]}
+        />
         <form onSubmit={this.handleSubmit} className="center">
           <br />
           <textarea
             onChange={this.handleInput}
             type="text"
             value={this.state.commentInput}
+            style={{ fontFamily: "IM Fell DW Pica, monospaced" }}
+            id="textInput"
           />
           <br />
-          <button>Post comment!</button>
+          <button
+            style={{ fontFamily: "IM Fell DW Pica SC, monospaced" }}
+            disabled={this.state.commentInput === ""}
+          >
+            Post comment
+          </button>
         </form>
+        {this.state.newPost && (
+          <div className="voter">
+            <FontAwesomeIcon id="voter" icon={faCloudRain} size="lg" />
+            <p style={{ fontFamily: "IM Fell DW Pica SC, monospaced" }}>
+              Comment posted!
+            </p>
+            <FontAwesomeIcon id="voter" icon={faCloudRain} size="lg" />
+          </div>
+        )}
       </div>
     );
   }
 
   handleInput = event => {
-    this.setState({ commentInput: event.target.value });
+    this.setState({ commentInput: event.target.value, newPost: false });
   };
 
   handleSubmit = event => {
@@ -34,7 +66,7 @@ class PostComment extends React.Component {
     postComment(id, username, commentInput).then(res =>
       updatePostedComments(res.data.comment)
     );
-    this.setState({ commentInput: "" });
+    this.setState({ commentInput: "", newPost: true });
   };
 }
 
