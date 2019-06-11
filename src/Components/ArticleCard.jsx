71f678bc83e590e705834baa8f88bf59d4885317
Nick Etherington <nicketherington@Nicks-MacBook.local>
2019-06-11 09:56:28 +0100

    removed getUser requests from comment and article cards following back-end update

--- src/Components/ArticleCard.jsx
+++ src/Components/ArticleCard.jsx
@@ -1,67 +1,51 @@
 import React from "react";
 import { Link } from "@reach/router";
 import GoogleFontLoader from "react-google-font-loader";
-import { getUser } from "../Api";
 import "../App.css";
 const moment = require("moment");
 
-class ArticleCard extends React.Component {
-  state = {
-    userAvatar: null
-  };
-
-  render() {
-    const article = this.props.article;
-    const date = moment(article.created_at);
-    const newArticle = (
-      <li>
-        <Link to={`/articles/${article.article_id}`}>
-          <GoogleFontLoader
-            fonts={[
-              {
-                font: "IM Fell DW Pica SC",
-                weights: [400, "400i"]
-              },
-              {
-                font: "IM Fell DW Pica",
-                weights: [400, "400i"]
-              }
-            ]}
-          />
-          <h3 style={{ fontFamily: "IM Fell DW Pica SC, monospaced" }}>
-            {article.title}
-          </h3>
-        </Link>
-        <p style={{ fontFamily: "IM Fell DW Pica, monospaced" }}>
-          Topic: {article.topic}
-        </p>
-        {this.state.userAvatar && (
-          <img className="avatar" alt="avatar" src={this.state.userAvatar} />
-        )}
-        <p style={{ fontFamily: "IM Fell DW Pica, monospaced" }}>
-          {article.author}
-        </p>
-        <p style={{ fontFamily: "IM Fell DW Pica, monospaced" }}>
-          {date._d.toString().slice(0, -34)}
-        </p>
-        <p style={{ fontFamily: "IM Fell DW Pica, monospaced" }}>
-          Votes: {article.votes}
-        </p>
-        <p style={{ fontFamily: "IM Fell DW Pica, monospaced" }}>
-          Comments: {article.comment_count}
-        </p>
-        <br />
-      </li>
-    );
-    return newArticle;
-  }
-
-  componentDidMount = () => {
-    const username = this.props.article.author;
-    getUser(username).then(user =>
-      this.setState({ userAvatar: user.avatar_url })
-    );
-  };
-}
+const ArticleCard = props => {
+  const article = props.article;
+  const date = moment(article.created_at);
+  const newArticle = (
+    <li>
+      <Link to={`/articles/${article.article_id}`}>
+        <GoogleFontLoader
+          fonts={[
+            {
+              font: "IM Fell DW Pica SC",
+              weights: [400, "400i"]
+            },
+            {
+              font: "IM Fell DW Pica",
+              weights: [400, "400i"]
+            }
+          ]}
+        />
+        <h3 style={{ fontFamily: "IM Fell DW Pica SC, monospaced" }}>
+          {article.title}
+        </h3>
+      </Link>
+      <p style={{ fontFamily: "IM Fell DW Pica, monospaced" }}>
+        Topic: {article.topic}
+      </p>
+      <img className="avatar" alt="avatar" src={article.avatar_url} />
+      <p style={{ fontFamily: "IM Fell DW Pica, monospaced" }}>
+        {article.author}
+      </p>
+      <p style={{ fontFamily: "IM Fell DW Pica, monospaced" }}>
+        {date._d.toString().slice(0, -34)}
+      </p>
+      <p style={{ fontFamily: "IM Fell DW Pica, monospaced" }}>
+        Votes: {article.votes}
+      </p>
+      <p style={{ fontFamily: "IM Fell DW Pica, monospaced" }}>
+        Comments: {article.comment_count}
+      </p>
+      <br />
+    </li>
+  );
+  return newArticle;
+};
 
 export default ArticleCard;
--- src/Components/CommentCard.jsx
+++ src/Components/CommentCard.jsx
@@ -1,56 +1,42 @@
 import React from "react";
 import Voter from "./Voter";
-import { deleteComment, getUser } from "../Api";
+import { deleteComment } from "../Api";
 import "../App.css";
 const moment = require("moment");
 
-class CommentCard extends React.Component {
-  state = {
-    userAvatar: null
-  };
-  render() {
-    const { comment, username, updateForDeletedComment } = this.props;
-    const { author, created_at, body, votes, comment_id } = comment;
-    const date = moment(created_at);
-    const newComment = (
-      <li>
-        {this.state.userAvatar && (
-          <img className="avatar" alt="avatar" src={this.state.userAvatar} />
-        )}
-        <h5 style={{ fontFamily: "IM Fell DW Pica, monospaced" }}>{author}</h5>
-        <h5 style={{ fontFamily: "IM Fell DW Pica, monospaced" }}>
-          {date._d.toString().slice(0, -34)}
-        </h5>
-        <p
-          style={{ fontFamily: "IM Fell DW Pica, monospaced" }}
-          className="commentText"
-        >
-          {body}
-        </p>
-        {username && <Voter votes={votes} id={comment_id} type="comment" />}
-        {username === author && (
-          <div className="center">
-            <button
-              style={{ fontFamily: "IM Fell DW Pica SC, monospaced" }}
-              onClick={() =>
-                deleteComment(comment_id).then(updateForDeletedComment)
-              }
-            >
-              Delete Comment
-            </button>
-          </div>
-        )}
-      </li>
-    );
-    return newComment;
-  }
-
-  componentDidMount = () => {
-    const username = this.props.comment.author;
-    getUser(username).then(user =>
-      this.setState({ userAvatar: user.avatar_url })
-    );
-  };
-}
+const CommentCard = props => {
+  const { comment, username, updateForDeletedComment } = props;
+  const { author, created_at, body, votes, comment_id, avatar_url } = comment;
+  const date = moment(created_at);
+  const newComment = (
+    <li>
+      <img className="avatar" alt="avatar" src={avatar_url} />
+      <h5 style={{ fontFamily: "IM Fell DW Pica, monospaced" }}>{author}</h5>
+      <h5 style={{ fontFamily: "IM Fell DW Pica, monospaced" }}>
+        {date._d.toString().slice(0, -34)}
+      </h5>
+      <p
+        style={{ fontFamily: "IM Fell DW Pica, monospaced" }}
+        className="commentText"
+      >
+        {body}
+      </p>
+      {username && <Voter votes={votes} id={comment_id} type="comment" />}
+      {username === author && (
+        <div className="center">
+          <button
+            style={{ fontFamily: "IM Fell DW Pica SC, monospaced" }}
+            onClick={() =>
+              deleteComment(comment_id).then(updateForDeletedComment)
+            }
+          >
+            Delete Comment
+          </button>
+        </div>
+      )}
+    </li>
+  );
+  return newComment;
+};
 
 export default CommentCard;
